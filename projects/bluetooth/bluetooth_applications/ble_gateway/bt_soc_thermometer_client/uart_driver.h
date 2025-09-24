500a5c5d2bbce669ddfb3206605f3da06335c31d
ZuyZ <luukhanhduy040903@gmail.com>
2025-09-24 14:23:49 +0700

    IOT_DS-12: add create frame function

--- projects/bluetooth/bluetooth_applications/ble_gateway/bt_soc_thermometer_client/uart_driver.h
+++ projects/bluetooth/bluetooth_applications/ble_gateway/bt_soc_thermometer_client/uart_driver.h
@@ -2,7 +2,12 @@
 #define UART_DRIVER_H
 #include "ecode.h"
 #include "uartdrv.h"
+#include <stdint.h>
 Ecode_t uart_transmit(const uint8_t *data, uint32_t length);
 Ecode_t uart_receive(uint8_t *data, uint32_t length);
 UARTDRV_Handle_t take_uart_handle(void);
+uint16_t uart_make_frame(uint8_t *buf,
+                         uint8_t opcode,
+                         uint8_t type,
+                         const uint8_t *value);
 #endif // UART_DRIVER_H
