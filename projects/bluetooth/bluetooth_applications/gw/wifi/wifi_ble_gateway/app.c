9a9d9284c8317a0832483c69f97f9fa8b60f4b91
fabian <nguyenthanhvinh1613@gmail.com>
2025-11-21 15:42:00 +0700

    IOT_DS-9: Uart mock tests

--- projects/bluetooth/bluetooth_applications/gw/wifi/wifi_ble_gateway/app.c
+++ projects/bluetooth/bluetooth_applications/gw/wifi/wifi_ble_gateway/app.c
@@ -1,27 +1,42 @@
 #include "uart_comm.h"
 #include "rsi_debug.h"
-#include "cmsis_os2.h"
 #include "string.h"
+
+// Macro to enable/disable mock test
+#define MOCK_TEST_UART 0
+
+void mock_test_uart(void);
+
 void app_init(void)
 {
-   uart_init();
+  uart_init();
 }
 
-
+/***************************************************************************/ /**
+ * App ticking function.
+ ******************************************************************************/
 void app_process_action(void)
 {
+#if MOCK_TEST_UART
+  mock_test_uart();
+#endif /*MOCK_TEST_UART*/
+}
+
+#if MOCK_TEST_UART
+void mock_test_uart(void)
+{
   // 1. Test uart_read_byte
-//  uint8_t data;
-//  uart_read_byte(&data, osWaitForever);
-//  DEBUGOUT ("%c  \r\n", data);
+  uint8_t received_data;
+  uart_read_byte(&received_data, osWaitForever);
+
 
   // 2. Test uart_write_bytes
-//   static bool sent = true;
-//
-//     if (sent) {
-//       uint8_t msg[] = "Hello from UART!\r\n";
-//       uint32_t len = strlen((char*)msg);
-//       uart_write_bytes(msg, len, osWaitForever);
-//       sent = false; // Stop sending
-//     }
+  static bool sent = true;
+  if (sent) {
+    uint8_t msg[] = "Hello from UART!\r\n";
+    uint32_t len = strlen((char*)msg);
+    uart_write_bytes(msg, len, osWaitForever);
+    sent = false; // Stop sending
+  }
 }
+#endif /*MOCK_TEST_UART*/
--- projects/bluetooth/bluetooth_applications/gw/wifi/wifi_ble_gateway/main.c
+++ projects/bluetooth/bluetooth_applications/gw/wifi/wifi_ble_gateway/main.c
@@ -8,7 +8,7 @@ int main(void)
 
   app_init();
 
-  while (1) {
+  while (sl_main_start_task_should_continue()) {
     app_process_action();
   }
 }
